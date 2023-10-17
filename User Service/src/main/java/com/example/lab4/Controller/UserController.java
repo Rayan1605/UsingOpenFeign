75603ae0a430e622a99fbe073896e5856ef4ede4
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-10-17 03:36:38 +0100

    Start

--- User Service/src/main/java/com/example/lab4/Controller/UserController.java
+++ User Service/src/main/java/com/example/lab4/Controller/UserController.java
@@ -1,4 +1,15 @@
 package com.example.lab4.Controller;
 
+import com.example.lab4.Interface.RegistrationServiceClient;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
 public class UserController {
+
+private RegistrationServiceClient registrationServiceClient;
+
+
+
 }
