3ba89efc536d22bafdf4166358ffa1aafc8a2020
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-10-17 03:32:49 +0100

    Start

---
+++ User Service/src/main/java/com/example/lab4/Entity/UserDetails.java
@@ -0,0 +1,4 @@
+package com.example.lab4.Entity;
+
+public class UserDetails {
+}
--- User Service/src/main/java/com/example/lab4/Interface/RegistrationServiceClient.java
+++ User Service/src/main/java/com/example/lab4/Interface/RegistrationServiceClient.java
@@ -1,7 +1,12 @@
 package com.example.lab4.Interface;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name = "confirmation-service",url = "https://localhost:8082")
 public interface RegistrationServiceClient {
+
+    @PostMapping("/confirm")
+    String someDetails();
+
 }
