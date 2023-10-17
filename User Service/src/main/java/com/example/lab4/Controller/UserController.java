e56972e329d05bbb30622cf5cc902377fa766285
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-10-17 03:37:19 +0100

    Start

--- User Service/src/main/java/com/example/lab4/Controller/UserController.java
+++ User Service/src/main/java/com/example/lab4/Controller/UserController.java
@@ -2,7 +2,10 @@ package com.example.lab4.Controller;
 
 import com.example.lab4.Interface.RegistrationServiceClient;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @AllArgsConstructor
@@ -11,5 +14,9 @@ public class UserController {
 private RegistrationServiceClient registrationServiceClient;
 
 
+@PostMapping("/confirm-and-register")
+    public Map<String,String> confirmAndRegister(){
+
+    }
 
 }
