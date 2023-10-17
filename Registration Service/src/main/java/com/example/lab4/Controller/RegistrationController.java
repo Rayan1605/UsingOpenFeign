a7ad43e1e61078fe57148ec33026bb18e6eba835
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-10-17 03:55:26 +0100

    Start

--- Registration Service/src/main/java/com/example/lab4/Controller/RegistrationController.java
+++ Registration Service/src/main/java/com/example/lab4/Controller/RegistrationController.java
@@ -1,4 +1,15 @@
 package com.example.lab4.Controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RegistrationController {
+
+@PostMapping("/register")
+public String register() {
+
+}
+
+
 }
