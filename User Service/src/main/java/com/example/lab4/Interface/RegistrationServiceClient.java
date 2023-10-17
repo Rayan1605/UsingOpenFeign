a9a282ae23a7f6792e042e24d54cdb935ae3a3a2
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-10-17 03:29:20 +0100

    Start

---
+++ User Service/src/main/java/com/example/lab4/Interface/RegistrationServiceClient.java
@@ -0,0 +1,4 @@
+package com.example.lab4.Interface;
+
+public class RegistrationServiceClient {
+}
--- User Service/src/main/java/com/example/lab4/Lab4Application.java
+++ User Service/src/main/java/com/example/lab4/Lab4Application.java
@@ -2,8 +2,10 @@ package com.example.lab4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients //This is to enable feign clients
 public class Lab4Application {
 
     public static void main(String[] args) {
