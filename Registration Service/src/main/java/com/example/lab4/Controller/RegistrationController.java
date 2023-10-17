060a812a5776178266d81f5f1cfbdcb2d491e7f7
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-10-17 03:55:43 +0100

    Start

--- Registration Service/src/main/java/com/example/lab4/Controller/RegistrationController.java
+++ Registration Service/src/main/java/com/example/lab4/Controller/RegistrationController.java
@@ -1,13 +1,16 @@
 package com.example.lab4.Controller;
 
+import com.example.lab4.Entity.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RegistrationController {
 
 @PostMapping("/register")
-public String register() {
+public String register(@RequestBody UserDetails userDetails){
+
 
 }
 
