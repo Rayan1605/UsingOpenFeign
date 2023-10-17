0696509cfc1a0a614f077a39a8f23a061ace85a9
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-10-17 03:30:32 +0100

    Start

--- User Service/src/main/java/com/example/lab4/Interface/RegistrationServiceClient.java
+++ User Service/src/main/java/com/example/lab4/Interface/RegistrationServiceClient.java
@@ -1,4 +1,7 @@
 package com.example.lab4.Interface;
 
-public class RegistrationServiceClient {
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(name = "confirmation-service",url = "https://localhost:8082")
+public interface RegistrationServiceClient {
 }
